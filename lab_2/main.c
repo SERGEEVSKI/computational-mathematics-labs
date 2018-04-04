905f5d9ff39b0d11b1f01a0c82017d5c3cc510da
sergeevski <sergeevski@outlook.com>
2018-04-04 22:57:18 +0700

    Changed num.1

--- lab_2/main.c
+++ lab_2/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 void print(float a[3][3], float b[3])
 {
@@ -9,29 +10,96 @@ void print(float a[3][3], float b[3])
                 for(j = 0; j < 3; j++)
                 {
                         if(a[i][j] >= 0)
-                                printf(" %.3f\t", a[i][j]);
+                                printf(" %.2f\t", a[i][j]);
                         else
-                                printf("%.3f\t", a[i][j]);
+                                printf("%.2f\t", a[i][j]);
                 }
                 if(b[i] >= 0)
-                        printf("|  %.3f\n", b[i]);
+                        printf("|  %.2f\n", b[i]);
                 else
-                        printf("| %.3f\n", b[i]);
+                        printf("| %.2f\n", b[i]);
         }
 
+}
+
+void udvid(float a[3][3], float b[3])
+{
+	int i, j;
+	float var = 0;
+	for(i = 0; i < 3; i++)
+	{
+		var = a[i][i];
+		for(j = 0; j < 3; j++)
+		{
+			a[i][j] = a[i][j] / var;
+		}
+		b[i] = b[i] / var;
+		var = 0;
+	}
+}
+
+void razb(float a[3][3])
+{
+	int i;
+	for(i = 0; i < 3; i++)
+	{
+		a[i][i] = 0;
+	}
+}
+
+void norma(float a[3][3], float b[3])
+{
+	float q, w, e, r;
+
+	q = fabs(a[0][1]) + fabs(a[0][2]);
+	w = fabs(a[1][0]) + fabs(a[1][2]);
+	e = fabs(a[2][0]) + fabs(a[2][1]);
+
+	if(q >= w && q >= e)
+		r = q;
+	else if(w >= q && w >= e)
+		r = w;
+	else if(e >= q && e >= w);
+		r = e;
+
+	printf("\n");
+
+	printf("||C||inf = max(|%.2f| + |%.2f| ; |%.2f| + |%.2f| ; |%.2f| + |%.2f|) = %.2f\n", a[0][1], a[0][2], a[1][0], a[1][2], a[2][0], a[2][1], r);
+
+	q = 0; w = 0; e = 0;
+	q = fabs(b[0]);
+        w = fabs(b[1]);
+        e = fabs(b[2]);
+
+        if(q >= w && q >= e)
+		r = q;
+	else if(w >= q && w >= e)
+		r = w;
+	else if(e >= q && e >= w)
+		r = e;
+
+	printf("||B||inf = max(|%.2f| ; |%.2f| ; |%.2f|) = %.2f\n", b[0], b[1], b[2], r);
 }
 
 int main()
 {
 	float a[3][3], b[3];
-	int i, j;
 
 	a[0][0] =  5; a[0][1] = -1; a[0][2] = -1; b[0] =  3;
 	a[1][0] = -1; a[1][1] = -3; a[1][2] =  0; b[1] = -7;
 	a[2][0] =  1; a[2][1] =  1; a[2][2] =  4; b[2] =  3;
 
+	print(a, b);
+
+	udvid(a, b);
 
 	print(a, b);
+
+	razb(a);
+
+	print(a, b);
+
+	norma(a, b);
 
 	return 0;
 }
